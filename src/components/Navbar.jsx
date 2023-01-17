1d2f3f39f5f7f011034a3982073b5e0a5588f500
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-17 09:12:23 -0300

    constants added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
+import { logo } from "../utils/constants";
+
 const Navbar = () => {
   return <div>Navbar</div>;
 };
