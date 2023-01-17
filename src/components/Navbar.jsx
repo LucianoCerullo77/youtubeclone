419eb1c9e9aea80490acd8bf565b79fb6af7b794
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-17 14:30:47 -0300

    stack styled in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,22 @@ import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: "0",
+        justifyContent: "space-between",
+      }}
+    >
+
+      
+    </Stack>
+  );
 };
 
 export default Navbar;
