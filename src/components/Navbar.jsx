604ffa8bec587b7b01bcb5db1bbf92e93919e7f2
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-19 10:53:41 -0300

    search bar component started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-
 import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
@@ -17,8 +17,9 @@ const Navbar = () => {
       }}
     >
       <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-        <img src={logo} alt="logo" height={45}/>
+        <img src={logo} alt="logo" height={45} />
       </Link>
+      <SearchBar/>
     </Stack>
   );
 };
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { searchIcon } from "@mui/icons-material";
+
+const SearchBar = () => {
+  return (
+    <Paper
+    
+    >
+
+    </Paper>
+  )
+};
+
+export default SearchBar;
