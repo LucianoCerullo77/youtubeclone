bbe9350dc4d71c489413b25fa9de4dbab35df1a0
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-21 10:17:21 -0300

    stack sidebar started, direction changed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
 
 const Sidebar = () => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      SideBar
+    </Stack>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
