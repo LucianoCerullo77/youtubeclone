8ec200500b0bb277b772b1efa4face0f1e27be65
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-21 10:45:05 -0300

    stylization updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,10 +18,18 @@ const Sidebar = () => {
           className="category-btn"
           style={{
             background: category.name === selectedCategory && "#fc1503",
-            color : 'white'
+            color: "white",
           }}
+          key={category.name}
         >
-          <span>{category.icon}</span>
+          <span
+            style={{
+              color: category.name === selectedCategory ? "white" : "red",
+              marginRight : '15px'
+            }}
+          >
+            {category.icon}
+          </span>
           <span>{category.name}</span>
         </button>
       ))}
