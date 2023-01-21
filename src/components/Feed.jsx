95504f2e6e4b6d1a6089aa16ae1ddd0a0d35cbff
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-20 21:23:05 -0300

    feed sidebar background maded

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,7 +10,16 @@ const Feed = () => {
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
         }}
-      ></Box>
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 YouWatch
+        </Typography>
+      </Box>
     </Stack>
   );
 };
