3fc7aff048eb5a567d15134d4c8f779eb8afd4cc
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-24 10:46:09 -0300

    card channel and video title updated

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -27,8 +27,22 @@ const VideoCard = ({
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography>
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle.slice(0, 60) ||
+              demoChannelTitle.slice(0, 60)}
+              <CheckCircle sx={{fontSize : 12, color : 'gray', ml:'5px'}}/>
           </Typography>
         </Link>
       </CardContent>
