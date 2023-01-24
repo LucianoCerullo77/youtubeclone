4214ea3be7dbaf041339a17277b3ef4fc968bd34
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-24 10:51:11 -0300

    video card updated

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,7 +17,7 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card>
+    <Card sx={{width:{md : '320px', sx: '100%'}, boxShadow:'none', borderRadius: 0}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
