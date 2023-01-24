fb4347e106f9eff1240fd3f98a2dbbdda2459522
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-24 11:01:22 -0300

    imports maded in channelcard, also constants updated to see the channelcard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
-};
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({channelDetail}) => (
+  <div>ChannelCard</div>
+)
 
 export default ChannelCard;
