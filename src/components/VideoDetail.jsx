20350803356f150d405a539f76f2290b62d157a1
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-28 11:14:58 -0300

    basic controls maded

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -23,7 +23,11 @@ const VideoDetail = () => {
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
           </Box>
         </Box>
       </Stack>
